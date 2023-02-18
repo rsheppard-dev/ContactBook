286cd52f1dd300d9e4e80fd226dcc546cf41506f
Roy Sheppard <rsheppard83@gmail.com>
2023-02-18 18:05:06 +0000

    fix connectionhelper

--- Helpers/ConnectionHelper.cs
+++ Helpers/ConnectionHelper.cs
@@ -21,7 +21,7 @@ public static class ConnectionHelper
     private static string BuildConnectionString(string databaseUrl)
     {
         var databaseUri = new Uri(databaseUrl);
-        var userInfo = databaseUri.UserInfo.Split(';');
+        var userInfo = databaseUri.UserInfo.Split(':');
         var builder = new NpgsqlConnectionStringBuilder
         {
             Host = databaseUri.Host,
