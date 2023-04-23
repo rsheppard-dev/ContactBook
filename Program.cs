b398bdc58b7c22798d21b7e9784cb981b50e744f
Roy Sheppard <rsheppard83@gmail.com>
2023-04-23 15:29:47 +0100

    db setup

--- Program.cs
+++ Program.cs
@@ -5,9 +5,9 @@ using ContactBook.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = builder.Configuration.GetSection("pgSettings")["pgConnection"];
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
